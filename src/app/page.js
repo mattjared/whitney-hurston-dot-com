d010de3c93540323520d0ed82a24fb84fea92ac1
Whitney Hurston <whitneyhurston@Whitneys-MBP.attlocal.net>
2023-06-14 09:59:24 -0400

    poop

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import Footer from "./Footer"
 
 export default function Home() {
   return (
-    <main className='bg-gray-100'>
+    <main className='bg-yellow-100'>
       <section className="py-10 md:pt-24 md:pb-12 container max-w-screen-xl mx-auto px-4 text-center">
-        <Image src="/headshot.jpeg" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
-        <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">{`my name`}</h1>
-        <h2 className='text-6xl font-bold'>Account Executive @ Vercel</h2>
-        <p className="font-normal text-gray-600 text-md md:text-xl my-16">Selling software and keepign it A1 since Day 1</p>
+        <Image src="/linkedinphoto.jpeg" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
+        <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">{`Whitney Hurston`}</h1>
+        <h2 className='text-6xl font-bold'>AE @ Vercel</h2>
+        <p className="font-bold text-red-600 text-md md:text-xl my-16">lalalala</p>
       </section>
       <Contact />
       <Tiles />
